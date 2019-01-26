1ebff206e4df84aa99373dee9ea8471e74c8420b
Austin Astorga <astorgaaustin@gmail.com>
2019-01-26 17:37:40 -0500

    Create Endpoint.swift

---
+++ Endpoint.swift
@@ -0,0 +1,74 @@
+//
+//  Endpoint.swift
+//  Cornell AppDev
+//
+//  Created by Austin Astorga on 11/27/18.
+//  Copyright © 2018 Austin Astorga. All rights reserved.
+//
+
+import Foundation
+
+
+enum EndpointMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+
+struct Endpoint {
+    let path: String
+    let queryItems: [URLQueryItem]
+    let headers: [String: String]
+    let body: Data?
+    let method: EndpointMethod
+}
+
+extension Endpoint {
+    /// POST initializer
+    init<T: Codable>(path: String, headers: [String: String] = [:], body: T) {
+        self.path = path
+        self.queryItems = []
+        self.headers = headers
+        self.method = .post
+
+        //Encode body
+        self.body = try? JSONEncoder().encode(body)
+    }
+
+    /// GET initializer
+    init(path: String, queryItems: [URLQueryItem] = [], headers: [String: String] = [:]) {
+        self.path = path
+        self.queryItems = queryItems
+        self.headers = headers
+        self.method = .get
+        self.body = nil
+    }
+}
+
+extension Endpoint {
+    // We still have to keep 'url' as an optional, since we're
+    // dealing with dynamic components that could be invalid.
+    var url: URL? {
+        var components = URLComponents()
+        components.scheme = "http"
+        components.host = "localhost"
+        components.path = path
+        components.port = 3000
+        components.queryItems = queryItems
+        return components.url
+    }
+
+
+    var urlRequest: URLRequest? {
+        guard let unwrappedURL = url else { return nil }
+        var request = URLRequest(url: unwrappedURL)
+        headers.forEach { (key, value) in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        request.httpMethod = method.rawValue
+        request.httpBody = body
+
+        return request
+    }
+}
+
